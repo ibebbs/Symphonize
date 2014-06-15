c0d01becba58d7b0445859c3f717362cc64a2ad7
ibebbs <github@bebbs.co.uk>
2014-06-15 10:06:20 +0100

    Adding solution to repository

--- Symphonize.For.Windows.Store/ViewModels/MainViewModel.cs
+++ Symphonize.For.Windows.Store/ViewModels/MainViewModel.cs
@@ -1,8 +1,72 @@
-﻿using Caliburn.Micro;
+﻿using Bebbs.Harmonize.With.Component;
+using Bebbs.Harmonize.With.Message;
+using Bebbs.Harmonize.With.Messaging.Client;
+using Bebbs.Harmonize.With.Messaging.Via.SignalR.Client;
+using Caliburn.Micro;
+using Symphonize.For.Windows.Store.Xaml;
+using System;
+using System.Reactive.Subjects;
+using System.Windows.Input;
 
 namespace Symphonize.For.Windows.Store.ViewModels
 {
     public class MainViewModel : Screen
     {
+        private static readonly IIdentity Identity = new Identity(Guid.NewGuid().ToString());
+
+        private string _connectionString = "http://localhost:8999";
+        private string _hubName = "HarmonizeHub";
+        private Subject<IMessage> _consumer;
+
+        private DelegateCommand _connect;
+
+        public MainViewModel()
+        {
+            _consumer = new Subject<IMessage>();
+            _connect = new DelegateCommand(ExecuteConnect);
+        }
+
+        private async void ExecuteConnect()
+        {
+            IEndpoint endpoint = Factory.Default.Create(_connectionString, _hubName);
+            await endpoint.Initialize();
+
+            Entity entity = new Entity(new Identity("Test"), null, null, null);
+
+            endpoint.Register(Identity, entity, _consumer);
+        }
+
+        public ICommand ConnectCommand
+        {
+            get { return _connect; }
+        }
+
+        public string ConnectionString
+        {
+            get { return _connectionString; }
+            set
+            {
+                if (value != _connectionString)
+                {
+                    _connectionString = value;
+
+                    NotifyOfPropertyChange(() => ConnectionString);
+                }
+            }
+        }        
+
+        public string HubName
+        {
+            get { return _hubName; }
+            set
+            {
+                if (value != _hubName)
+                {
+                    _hubName = value;
+
+                    NotifyOfPropertyChange(() => ConnectionString);
+                }
+            }
+        }
     }
 }
---
+++ Symphonize.For.Windows.Store/Xaml/DelegateCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Symphonize.For.Windows.Store.Xaml
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute ?? new Func<bool>(() => true);
+        }
+
+        public void NotifyCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
